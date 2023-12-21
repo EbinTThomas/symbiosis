4c8a19fb0ec6022ce9b5b387e5ff37455225219b
EbinTThomas <ebinkailath@gmail.com>
2023-12-21 22:18:08 +0530

    fixed shop price issue

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
-// const BASE_URL = 'http://127.0.0.1:8000'
+const BASE_URL = 'http://127.0.0.1:8000'
 // const BASE_URL = 'http://13.48.130.144:8000'
-const BASE_URL = 'https://demo.symbiosistrading.com'
+// const BASE_URL = 'https://demo.symbiosistrading.com'
 
 export default axios.create({
   baseURL: BASE_URL,
